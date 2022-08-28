57fb9ed77182b79a5a14059e5a3a633c09c0da32
ketans2141 <105305997+ketans2141@users.noreply.github.com>
2022-08-28 21:39:49 +0530

    Update index.js

--- Assignment_Test/index.js
+++ Assignment_Test/index.js
@@ -2,6 +2,7 @@ const input = document.querySelector("#input");
 const searchBtn = document.querySelector(".search_btn");
 const main = document.querySelector(".main");
 
+
 searchBtn.addEventListener("click", function (name) {
     main.innerHTML="";
   fetch(` https://api.tvmaze.com/search/shows?q=${input.value}`)
@@ -10,16 +11,34 @@ searchBtn.addEventListener("click", function (name) {
       console.log(data);
       for (let i = 0; i < data.length; i++) {
         let result = `<div class="box">
-        // <img src="${data[i]["show"]["image"].medium}" >
+         <img src="${data[i]["show"]["image"].medium}" >
         <div>
            <h4>Name:${data[i]["show"].name}</h4>
            <h4>Start Date: ${data[i]["show"].premiered}</h4>
            <h4>Language:${data[i]["show"].language}</h4>
         </div>
         <button class="show_details">Show Details</button>
-    </div>`;
-
+    </div>`
         main.innerHTML += result;
       }
-    });
-});
+
+      let showDetails=document.querySelector(".show_details");
+    
+      showDetails.addEventListener("click",function(){
+        
+
+        fetch(` https://api.tvmaze.com/shows/1?embed[]=seasons&embed[]=cast`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(showDetails);
+
+          
+        let finalresult=`<div class="DetailsDiv">
+        `
+      });
+
+    })  
+
+})
+
+})
